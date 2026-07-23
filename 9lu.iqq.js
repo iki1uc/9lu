5e57fcc00ba1299b268b9619e73f65bb2febd93d
iki1uc <entertrainer@web.de>
2026-07-23 11:32:45 +0200

    Update 9lu.iqq.js

--- 9lu.iqq.js
+++ 9lu.iqq.js
@@ -3,3 +3,11 @@ export const 9lu = {
   max: v => ["9lu","MAX",v],
   edit: v => ["9lu","EDIT",v]
 };
+export const 9lu = {
+  min: v => ["9lu","MIN",v],
+  max: v => ["9lu","MAX",v],
+  edit: v => ["9lu","EDIT",v],
+  room: v => ["9lu","ROOM",v],
+  view: v => ["9lu","VIEW",v],
+  impulse: v => ["9lu","IMPULSE",v]
+};
